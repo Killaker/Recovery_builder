285eace96352649ee256019f4f7cf37a09baa2fb
ema alvarez <ema1986ct@gmail.com>
2023-03-26 06:30:26 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,13 +3,13 @@
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
 export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export BRANCH="twrp-8.1" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export DEVICE="jeice" # codename used in device tree
+export DT_LINK="https://github.com/Killaker/android_device_bq_jeice.git" # device tree link
+export DT_BRANCH="jeice_bq-user-8.1.0-OPM1.171019.026-2419-release-keys" # device tree branch
+export VENDOR="bq" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
