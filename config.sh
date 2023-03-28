8c43e99812f87e70ad2021da7c7dfc46b91c115b
ema alvarez <ema1986ct@gmail.com>
2023-03-28 19:59:57 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,11 +7,11 @@ export BRANCH="twrp-8.1" # the branch of manifest
 
 # about your device
 export DEVICE="jeice" # codename used in device tree
-export DT_LINK="https://github.com/Killaker/android_device_bq_jeice" # device tree link
+export DT_LINK="https://github.com/Killaker/android_device_bq_jeice.git" # device tree link
 export DT_BRANCH="main" # device tree branch
 export VENDOR="bq" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="omni_" # recovery type: omni, twrp, something else
+export TYPE="omni" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
 export SYM="_"
